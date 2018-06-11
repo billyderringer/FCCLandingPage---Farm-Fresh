be0d6c668355c77579ece5bf042e179f941b23c8
Billy Derringer <billyderringer7@gmail.com>
2018-06-11 16:43:24 -0500

    made product card images larger

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ makeProducts = () => {
         info = document.createElement('p')
 
         title.innerHTML = products[i].name
-        info.innerHTML = 'Fresh picked <br/><br/>' + products[i].name + '<br/><br/>$' + products[i].price
+        info.innerHTML = 'Fresh picked' + products[i].name + '$' + products[i].price
 
         //set styles
         infoContainer.className = 'product-info center-text'
